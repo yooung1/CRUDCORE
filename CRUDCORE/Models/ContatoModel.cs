055d3ba361e1fd942c27b666b33fd79be501af61
Samuel Pierre <samuelpierrej@gmail.com>
2024-09-23 16:57:56 -0300

    Add project files.

---
+++ CRUDCORE/Models/ContatoModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUDCORE.Models
+{
+    /// <summary>
+    /// Classe para CRUD no banco de dados - CREATE/UPDATE/DELETE
+    /// SEMPRE QUE CHAMAR ESTA CLASSE IREI CRIEI UM OBJ CONTENDO OS DADOS QUE SERÃO SOLICITADOS
+    /// </summary>
+    public class ContatoModel
+    {
+        public int IdContato { get; set; }
+        public string? Nome { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+    }
+}
